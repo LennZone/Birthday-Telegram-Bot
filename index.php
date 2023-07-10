bde5a0aaaefc8e3156b169acbc7a1f7e3724f082
LennZone <lennart.novak.abtei@gmail.com>
2023-07-10 18:37:56 +0200

    Add while true loop

--- index.php
+++ index.php
@@ -3,39 +3,41 @@
 $botToken = 'bot_token';
 $chatId = 'group_id'; //telegram group
 $apiUrl = "https://api.telegram.org/bot{$botToken}/sendMessage";
+$currentDate = date('Y-m-d');
 
 $users = json_decode(file_get_contents('users.json'), true);
 
-$currentDate = date('Y-m-d');
+while (true) {
+    foreach ($users as $user) {
+        $name = $user['name'];
+        $birthdate = $user['birthdate'];
 
+        if (substr($birthdate, 5) === substr($currentDate, 5)) {
+            $message = "Herzlichen Glückwunsch, {$name}! Alles Gute zum Geburtstag!";
 
-foreach ($users as $user) {
-    $name = $user['name'];
-    $birthdate = $user['birthdate'];
+            $data = [
+                'chat_id' => $chatId,
+                'text' => $message
+            ];
 
-    if (substr($birthdate, 5) === substr($currentDate, 5)) {
-        $message = "Herzlichen Glückwunsch, {$name}! Alles Gute zum Geburtstag!";
+            $options = [
+                'http' => [
+                    'method' => 'POST',
+                    'header' => 'Content-Type: application/x-www-form-urlencoded',
+                    'content' => http_build_query($data)
+                ]
+            ];
 
-        $data = [
-            'chat_id' => $chatId,
-            'text' => $message
-        ];
+            $context = stream_context_create($options);
+            $result = file_get_contents($apiUrl, false, $context);
 
-        $options = [
-            'http' => [
-                'method' => 'POST',
-                'header' => 'Content-Type: application/x-www-form-urlencoded',
-                'content' => http_build_query($data)
-            ]
-        ];
-
-        $context = stream_context_create($options);
-        $result = file_get_contents($apiUrl, false, $context);
-
-        if ($result === false) {
-            echo "Fehler beim Senden der Glückwunsch-Nachricht an {$name}.";
-        } else {
-            echo "Glückwunsch-Nachricht erfolgreich an {$name} gesendet!";
+            if ($result === false) {
+                echo "Fehler beim Senden der Glückwunsch-Nachricht an {$name}.";
+            } else {
+                echo "Glückwunsch-Nachricht erfolgreich an {$name} gesendet!";
+            }
         }
     }
+
+    sleep(3600);
 }
